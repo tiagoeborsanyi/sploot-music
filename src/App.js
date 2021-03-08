28be9071063b6edcd3a836311068400c9ba24114
Tiago Emerick <tiagoeborsanyi@gmail.com>
2021-03-08 18:30:29 -0300

    created body smartphone

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
-import './App.css';
+import './App.styles.scss';
+import { BsBatteryFull } from "react-icons/bs";
+import { AiFillSignal, AiOutlineWifi } from "react-icons/ai";
 
 function App() {
   return (
-    <div className="App">
-      inicio
+    <div className="app">
+      <div className='app__container'>
+        <div className='bar-info'>
+          <span className='bar-info__hour'>9:41</span>
+          <div className='bar-info__status'>
+            <ul className='bar-info__status--list'>
+              <li className='bar-info__status--item'>
+                <AiFillSignal />
+              </li>
+              <li className='bar-info__status--item'>
+                <AiOutlineWifi />
+              </li>
+              <li className='bar-info__status--item'>
+                <BsBatteryFull />
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div>
+
+        </div>
+      </div>
     </div>
   );
 }
